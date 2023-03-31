b518049c1ba0a0110ab891412357e6f3e165affe
Brandon Paulson <brandonpaulson28@gmail.com>
2023-03-30 20:07:16 -0500

    I have added in a header, footer, and an image of a cat

---
+++ assignment/Lecture 2.js
@@ -0,0 +1,70 @@
+// let greeting = 'Hello, World';
+// let children = 3; 
+// let hadDinner = false;
+
+// console.log(hadDinner);
+
+// hadDinner = true; 
+// // console.log(hadDinner); 
+
+// // let nothing = null;
+// // let alsoNothing =  undefined;
+
+// children = children + 1
+// console.log(children)
+
+// children += 1;
+// console.log(children)
+
+// children++;
+// console.log(children)
+// // This adds +1
+
+// children *= 2;
+// console.log(children)
+// //This multiplies b2 
+
+// children /= 3; 
+// console.log(children)
+// // This divides by 3
+
+// console.log(children % 2);
+// //This identifies essentially if it is an odd number
+
+// console.log('Java' + 'Script')
+
+// console.log(greeting + 'For Realz!')
+
+// console.log('Children', children)
+
+// let subtitle= document.querySelector('Welcome Paragraph');
+// subtitle.innerHTML = 'I love JavaScript'
+let children = 3
+// let children = 10
+if (children > 4) {
+    console.log('I have lots of kids!')
+    console.log('Yessir')
+} else if (children > 2){
+    console.log('I have medium kids')
+}
+else{
+    console.log('I do not have lots of kids!')
+
+}
+
+let ducks = 10;
+
+if (ducks <= 100) {
+     console.log('That is not enough ducks')
+}
+
+//Logical operators
+// Can use || to make an either or condition
+if(ducks > 80 && ducks < 120) {
+    console.log('Perfect Number of Ducks')
+}
+// Using a ! 
+let Name = 'Sue'
+if(Name === 'Sue' || Name === 'Bob'){
+    console.log('Hello Friend')
+}
