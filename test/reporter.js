9717eeb5b9ff7e584570f2af60f1d67244406dfe
Christopher Black <chris@primeacademy.io>
2023-08-14 21:15:22 -0500

    Added tests for week 1 assignment.

---
+++ test/reporter.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Mocha = require('mocha');
+const {
+    EVENT_RUN_BEGIN,
+    EVENT_RUN_END,
+    EVENT_TEST_FAIL,
+    EVENT_TEST_PASS,
+} = Mocha.Runner.constants;
+
+// Custom output to match our rubric
+class MyReporter {
+    constructor(runner) {
+        const stats = runner.stats;
+        const errors = [];
+        runner
+            .on(EVENT_TEST_FAIL, (test, err) => {
+                errors.push(err);
+                console.log(
+                    `\x1b[31m`,
+                    `| ${test.parent.title} | no, see note ${errors.length} |`,
+                    `\x1b[0m`
+                );
+                // console.log(test)
+            })
+            .on(EVENT_TEST_PASS, (test, err) => {
+                console.log(
+                    `\x1b[32m`,
+                    `| ${test.parent.title} | yes |`,
+                    `\x1b[0m`
+                );
+            })
+            .once(EVENT_RUN_BEGIN, () => {
+                console.log('');
+                console.log(`---
+| HTML, CSS, & JS | Complete? |
+| --- | :---: |`);
+                console.log(
+                    `\x1b[32m`,
+                    `| Checkpoints complete | - |`,
+                    `\x1b[0m`
+                );
+                console.log(
+                    `\x1b[32m`,
+                    `| JS contains console log | - |`,
+                    `\x1b[0m`
+                );
+                console.log(
+                    `\x1b[32m`,
+                    `| The correct repo was turned in | - |`,
+                    `\x1b[0m`
+                );
+                console.log(
+                    `\x1b[32m`,
+                    `| GitHub config correct | - |`,
+                    `\x1b[0m`
+                );
+                console.log(
+                    `\x1b[32m`,
+                    `| Runs in browser without console errors | yes |`,
+                    `\x1b[0m`
+                );
+            })
+            .once(EVENT_RUN_END, () => {
+                console.log('');
+                // console.log(`end: ${stats.passes}/${stats.passes + stats.failures} ok`);
+
+                errors.map((err, i) => {
+                    console.log('');
+                    console.log(`${i + 1}) ${err}`);
+                });
+            });
+    }
+}
+
+module.exports = MyReporter;
---
+++ test/test.js
@@ -0,0 +1,126 @@
+/**
+ * We have to do a bit of work upfront to allow the tests
+ * to run in the browser and in Node.js. 
+ */
+let assert, expect;
+let root;
+if (typeof window === 'object') {
+    // Run tests in browser
+    assert = chai.assert;
+    expect = chai.expect;
+    mocha.setup('bdd');
+    root = document;
+} else {
+    // Run tests in Node.js
+    assert = require('assert');
+    expect = require('chai').expect;
+    // get filesystem module
+    const fs = require("fs");
+    const { parse } = require('node-html-parser');
+    // using the readFileSync() function and passing the path to the file
+    const buffer = fs.readFileSync('assignment/index.html');
+    // use the toString() method to convert Buffer into String
+    const fileContent = buffer.toString();
+    root = parse(fileContent);
+}
+
+/**
+ * Put all tests within this describe.
+ */
+describe('Automated tests', function () {
+    describe('Create a header element `<header>` element', function () {
+        it('Create a header element `<header>` element', function () {
+            let header = root.querySelector('header');
+            expect(header, '<header> element not found.').to.exist;
+        });
+    });
+    describe('Inside the `<header>`, add an `<h1>` element containing your name', function () {
+        it('Inside the `<header>`, add an `<h1>` element containing your name', function () {
+            let header = root.querySelector('header');
+            expect(header, '<header> element not found.').to.exist;
+            let h1 = header.querySelector('h1');
+            expect(h1, '<h1> not in header.').to.exist;
+            expect(h1.innerHTML, '<h3> was empty.').not.to.be.empty;
+        });
+    });
+    describe('Background color added to the `<header>``', function () {
+        it('Background color added to the `<header>`', function () {
+            if (typeof window === 'object') {
+                let header = root.querySelector('header');
+                expect(header, '<header> element was not found in div.').to.exist;
+                let style = window.getComputedStyle(header);
+                let backgroundColor = style.getPropertyValue('background-color');
+                expect(backgroundColor, '<header> element does not have background color.').not.to.be.empty;
+                expect(backgroundColor, '<header> element does not have background color.').to.not.equal('rgba(0, 0, 0, 0)');
+            } else {
+                // Unable to test background color with node
+                this.skip();
+            }
+        });
+    });
+    describe('Background color added to the `<main>``', function () {
+        it('Background color added to the `<main>`', function () {
+            if (typeof window === 'object') {
+                let main = root.querySelector('main');
+                expect(main, '<main> element was not found in div.').to.exist;
+                let style = window.getComputedStyle(main);
+                let backgroundColor = style.getPropertyValue('background-color');
+                expect(backgroundColor, '<main> element does not have background color.').not.to.be.empty;
+                expect(backgroundColor, '<main> element does not have background color.').to.not.equal('rgba(0, 0, 0, 0)');
+            } else {
+                // Unable to test background color with node
+                this.skip();
+            }
+        });
+    });
+    describe('Include an image', function () {
+        it('Include an image', function () {
+            let div = root.querySelector('img');
+            expect(div, '<img> not found').to.exist;
+        });
+    });
+    describe('Create an unordered list with 3 list items of fun facts about you', function () {
+        it('Create an unordered list with 3 list items of fun facts about you', function () {
+            let ul = root.querySelector('main ul');
+            expect(ul, '<ul> element was not found.').to.exist;
+            let liCount = ul.querySelectorAll('li').length
+            expect(liCount, `${liCount} of 3 list items found`).to.be.greaterThan(2);
+        });
+    });
+    describe('Create a `<footer>` element', function () {
+        it('Create a `<footer>` element', function () {
+            let footer = root.querySelector('footer');
+            expect(footer, '<footer> element was not found.').to.exist;
+        });
+    });
+    describe('Inside your `<footer>`, put a `<p>` element with your favorite quote', function () {
+        it('Inside your `<footer>`, put a `<p>` element with your favorite quote', function () {
+            let footer = root.querySelector('footer');
+            expect(footer, '<footer> element was not found.').to.exist;
+            let p = footer.querySelector('p');
+            expect(p.innerHTML, '<h3> was empty.').not.to.be.empty;
+        });
+    });
+    describe('Background color added to the `<footer>``', function () {
+        it('Background color added to the `<footer>`', function () {
+            if (typeof window === 'object') {
+                let footer = root.querySelector('footer');
+                expect(footer, '<footer> element was not found in div.').to.exist;
+                let style = window.getComputedStyle(footer);
+                let backgroundColor = style.getPropertyValue('background-color');
+                expect(backgroundColor, '<footer> element does not have background color.').not.to.be.empty;
+                expect(backgroundColor, '<footer> element does not have background color.').to.not.equal('rgba(0, 0, 0, 0)');
+            } else {
+                // Unable to test background color with node
+                this.skip();
+            }
+        });
+    });
+});
+
+/**
+ * If running the tests in the browser, call mocha.run()
+ */
+if (typeof window === 'object') {
+    mocha.run();
+}
